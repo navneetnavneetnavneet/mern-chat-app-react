1e66418ae7837ed47b292aceed95dd4c8e5c8008
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-08-16 00:02:35 +0530

    update socket and axios base URLs to use environment variable for improved flexibility

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -21,7 +21,7 @@ export const SocketContextProvider = ({ children }) => {
     }
 
     // Initialize the socket connection
-    const newSocket = io("http://localhost:8080");
+    const newSocket = io(`${import.meta.env.VITE_API_URL}`);
     setSocket(newSocket);
 
     // Setup the socket for the current user
